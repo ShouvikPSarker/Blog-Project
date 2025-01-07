196a495ea772f651c43c7f8e3c38ade5e71eacc7
ShouvikPSarker <shouviksarker94@gmail.com>
2025-01-07 21:04:05 +0530

    Index component updation

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,10 +3,12 @@ import Footer from "./Footer";
 import Container from "./Container/Container";
 import Logout from "./Header/Logout";
 import Logo from "./Logo";
+import RTE from "./RTE";
 export{
     Header , 
     Footer , 
     Container , 
     Logout,
     Logo,
+    RTE,
 }
